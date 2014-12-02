1cf707b90da3db7dafad78768eb5b7293cb9213c
lianguasth <lianchengliang@LianChengliangs-MacBook-Pro.local>
2014-12-02 12:07:50 -0500

    revised eigen vector

--- lib/plotAll.py
+++ lib/plotAll.py
@@ -312,10 +312,10 @@ def plotEigenRev(dirs, path1, path2, plt, fig, ax):
 				tcol = int(words[1])
 				tval = float(words[2])
 				vectors.append(tval)
-		#num = int(len(vectors)/4)
-		num = int(len(vectors)/3)
+		num = int(len(vectors)/4)
+		#num = int(len(vectors)/3)
 		vectorR = []
-		for i in range(3):
+		for i in range(4):
 			t = []
 			for j in range(num):
 				t.append(vectors[i*num + j])
@@ -323,12 +323,12 @@ def plotEigenRev(dirs, path1, path2, plt, fig, ax):
 		minTotal = 100000000
 		#print len(vectors)
 		#for i in range(0,3):
-		for i in range(0,2):
+		for i in range(0,3):
 			xi = vectorR[i]
 			#print xi
 			#raw_input()
 			#for j in range(i+1, 4):
-			for j in range(i+1, 3):
+			for j in range(i+1, 4):
 				xj = vectorR[j]
 				#print xj
 				#raw_input()
